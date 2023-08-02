c19abbbeb1bc716f6375c936fc0c251623402dda
Jordan Godwin <jordan.godwin13@gmail.com>
2023-08-02 05:37:58 -0500

    Write code. Drink coffee. Finally started the PHP server.

--- Chapter_03/03_03/ajax_json/new_content.php
+++ Chapter_03/03_03/ajax_json/new_content.php
@@ -4,4 +4,6 @@
     'regular' => 'This is the new content which has been loaded by Ajax.',
     'long' => 'This content is the result of making an Ajax query to a PHP page which dynamically generated text as a response.'
   );
+
+  echo json_encode($content);
 ?>
