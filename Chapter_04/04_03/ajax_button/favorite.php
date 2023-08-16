0a87067362e4937d24d2f4ad473d54f5c09be0a1
Jordan Godwin <jordan.godwin13@gmail.com>
2023-08-16 05:59:12 -0500

    Add code to process regex blog id.

--- Chapter_04/04_03/ajax_button/favorite.php
+++ Chapter_04/04_03/ajax_button/favorite.php
@@ -16,9 +16,22 @@
   // extract $id
   $raw_id = isset($_POST['id']) ? $_POST['id'] : '';
 
-  echo $raw_id;
+  if(preg_match("/blog-post-(\d+)/", $raw_id, $matches)) {
+    $id = $matches[1];
+
+    if(!in_array($id, $_SESSION['favorites'])) {
+      $_SESSION['favorites'][] = $id;
+    }
+
+    echo 'true' ;
+  } else {
+    echo 'false';
+  }
+
+  
   
   // store in $_SESSION['favorites']
   // return true/false
+  echo 
 
 ?>
